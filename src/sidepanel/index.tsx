b9fb92c052cbda33c37adcd8b86a52f21adf6519
drizzle <drizzlemisty@users.noreply.github.com>
2025-02-28 16:53:16 +0800

    style: side panel

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -7,7 +7,7 @@ const routes = getRoutes('popup');
 // The Side Panel allows extensions to display their own UI in the side panel,
 // enabling persistent experiences that complement the user's browsing journey.
 function IndexSidePanel() {
-    return <SinglePageApp className="flex h-full min-h-screen w-screen flex-col" routes={routes} />;
+    return <SinglePageApp className="flex h-screen w-screen flex-col" routes={routes} />;
 }
 
 export default IndexSidePanel;
