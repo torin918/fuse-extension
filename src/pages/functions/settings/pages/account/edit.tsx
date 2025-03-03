bde6fc3ee8d8d416902e5afe8337b1b0f9c8d3bf
judy <judyyan1101@gmail.com>
2025-03-03 16:18:29 +0800

    fix style

--- src/pages/functions/settings/pages/account/edit.tsx
+++ src/pages/functions/settings/pages/account/edit.tsx
@@ -1,11 +1,16 @@
+import { Button, Drawer, DrawerBody, DrawerContent, useDisclosure } from '@heroui/react';
 import { useMemo, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import { useParams, type NavigateOptions } from 'react-router-dom';
 
+import Icon from '~components/icon';
 import { FusePage } from '~components/layouts/page';
 import { FusePageTransition } from '~components/layouts/transition';
+import { showToast } from '~components/toast';
 import { useCurrentState } from '~hooks/memo/current_state';
 import { useGoto } from '~hooks/memo/goto';
 import { useIdentityKeys } from '~hooks/store/local-secure';
+import { truncate_text } from '~lib/utils/text';
 
 import { FunctionHeader } from '../../../components/header';
 
@@ -53,15 +58,97 @@ const InnerSingleAccountPage = ({
     const [mnemonic, setMnemonic] = useState<string>();
     const [private_key, setPrivateKey] = useState<string>();
 
+    const [isOpenRemove, setIsOpenRemove] = useState(false);
+    const removeAccount = async () => {
+        const r = await deleteIdentity(current.id, '1111qqqq');
+        console.error('delete identity', r);
+        if (r === undefined) return;
+        if (r === false) throw new Error('can not delete');
+        // notice successful
+        _goto(-1);
+    };
+
     if (!current || !identity_list) return <></>;
     return (
-        <div>
-            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-[#333333] p-2 text-2xl">
-                {current.icon}
+        <div className="w-full px-5">
+            <div className="flex items-center justify-center py-7">
+                <div className="flex h-[60px] w-[60px] items-center justify-center rounded-full bg-[#333333] text-4xl">
+                    {current.icon}
+                </div>
             </div>
-            <div>Account Name: {current.name}</div>
-            <div>Account Address: {current.address.ic?.owner}</div>
-            <div>Account Address: {current.address.ic?.account_id}</div>
+            <div className="w-full overflow-hidden rounded-xl bg-[#181818]">
+                <div className="flex cursor-pointer items-center justify-between border-b border-[#222222] px-4 py-3 duration-300 hover:bg-[#333333]">
+                    <span className="text-sm text-[#EEEEEE]">Account Name</span>
+                    <div className="flex items-center justify-between">
+                        <span className="text-sm text-[#999999]">{current.name}</span>
+                        <Icon name="icon-arrow-right" className="ml-3 h-3 w-3 text-[#999999]" />
+                    </div>
+                </div>
+                <div className="flex items-center justify-between border-b border-[#222222] px-4 py-3">
+                    <span className="text-sm text-[#EEEEEE]">Principal ID</span>
+                    <CopyToClipboard
+                        text={current.address.ic?.owner}
+                        onCopy={() => {
+                            showToast('Copied', 'success');
+                        }}
+                    >
+                        <div className="flex items-center justify-between">
+                            <span className="text-sm text-[#999999]">
+                                {truncate_text(current.address.ic?.owner || '')}
+                            </span>
+                            <Icon
+                                name="icon-copy"
+                                className="ml-3 h-3 w-3 cursor-pointer text-[#999999] duration-300 hover:text-[#FFCF13]"
+                            />
+                        </div>
+                    </CopyToClipboard>
+                </div>
+                <div className="flex items-center justify-between px-4 py-3">
+                    <span className="text-sm text-[#EEEEEE]">Account ID</span>
+                    <CopyToClipboard
+                        text={current.address.ic?.account_id}
+                        onCopy={() => {
+                            showToast('Copied', 'success');
+                        }}
+                    >
+                        <div className="flex items-center justify-between">
+                            <span className="text-sm text-[#999999]">
+                                {truncate_text(current.address.ic?.account_id || '')}
+                            </span>
+                            <Icon
+                                name="icon-copy"
+                                className="ml-3 h-3 w-3 cursor-pointer text-[#999999] duration-300 hover:text-[#FFCF13]"
+                            />
+                        </div>
+                    </CopyToClipboard>
+                </div>
+            </div>
+
+            <div className="mt-4 w-full overflow-hidden rounded-xl bg-[#181818]">
+                <div className="flex cursor-pointer items-center justify-between border-b border-[#222222] px-4 py-3 duration-300 hover:bg-[#333333]">
+                    <span className="text-sm text-[#EEEEEE]">Backup Seed Phrase</span>
+                    <Icon name="icon-arrow-right" className="ml-3 h-3 w-3 text-[#999999]" />
+                </div>
+                <div className="flex cursor-pointer items-center justify-between px-4 py-3 duration-300 hover:bg-[#333333]">
+                    <span className="text-sm text-[#EEEEEE]">Backup Private Key</span>
+                    <Icon name="icon-arrow-right" className="ml-3 h-3 w-3 text-[#999999]" />
+                </div>
+            </div>
+
+            {current.deletable && (
+                <div className="mt-4 w-full overflow-hidden rounded-xl bg-[#181818]">
+                    <span
+                        className="block cursor-pointer px-4 py-3 text-sm text-[#FF2C40] duration-300 hover:opacity-85"
+                        onClick={() => {
+                            setIsOpenRemove(true);
+                        }}
+                    >
+                        Remove Account
+                    </span>
+                </div>
+            )}
+            <RemoveAccount isOpen={isOpenRemove} setIsOpen={setIsOpenRemove} onDelete={removeAccount} />
+
             {current.key.type === 'mnemonic' && (
                 <>
                     <div
@@ -107,21 +194,64 @@ const InnerSingleAccountPage = ({
             >
                 Set Name
             </div>
-            {current.deletable && (
-                <div
-                    onClick={() => {
-                        deleteIdentity(current.id, '1111qqqq').then((r) => {
-                            console.error('delete identity', r);
-                            if (r === undefined) return;
-                            if (r === false) throw new Error('can not delete');
-                            // notice successful
-                            _goto(-1);
-                        });
-                    }}
-                >
-                    Remove Account
-                </div>
-            )}
         </div>
+    );
+};
+
+const RemoveAccount = ({
+    isOpen,
+    setIsOpen,
+    onDelete,
+}: {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+    onDelete: () => Promise<void>;
+}) => {
+    const { onOpenChange } = useDisclosure();
+
+    const handleClose = async () => {
+        try {
+            await onDelete();
+            setIsOpen(false);
+        } catch (error) {
+            console.error('Failed to delete account:', error);
+        }
+    };
+
+    return (
+        <Drawer isOpen={isOpen} placement="bottom" onOpenChange={onOpenChange}>
+            <DrawerContent>
+                <DrawerBody>
+                    <div className="fixed bottom-0 left-0 top-[60px] z-20 flex w-full flex-col justify-between border-t border-[#333333] bg-[#0a0600] px-5 pb-5">
+                        <div className="mt-8 flex w-full flex-col items-center justify-center py-10">
+                            <div className="flex h-[80px] w-[80px] items-center justify-center rounded-full bg-[#271511]">
+                                <Icon name="icon-remove" className="h-10 w-10 text-[#FF2C40]" />
+                            </div>
+                            <div className="w-full py-3 text-center text-lg text-[#FF2C40]">
+                                Confirm removal of wallet1?
+                            </div>
+                            <p className="text-sm text-[#FFCF13]">
+                                Even if you delete this wallet, you can still restore the account using the mnemonic
+                                phrase or private key of this wallet.
+                            </p>
+                        </div>
+                        <div className="flex items-center gap-x-3">
+                            <Button
+                                className="h-[48px] w-full text-lg font-semibold text-black"
+                                onPress={() => setIsOpen(false)}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                className="h-[48px] w-full bg-[#FFCF13] text-lg font-semibold text-black"
+                                onPress={handleClose}
+                            >
+                                Close
+                            </Button>
+                        </div>
+                    </div>
+                </DrawerBody>
+            </DrawerContent>
+        </Drawer>
     );
 };
