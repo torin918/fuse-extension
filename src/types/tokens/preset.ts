07b3954246bac3ef18169dbdbf10719026d78e9f
drizzle <drizzlemisty@users.noreply.github.com>
2025-03-06 15:08:44 +0800

    update ck Tokens

--- src/types/tokens/preset.ts
+++ src/types/tokens/preset.ts
@@ -1,26 +1,44 @@
+// prettier-ignore // cSpell:words eurc
 import { is_same_token_info, match_combined_token_info, TokenTag, type CombinedTokenInfo, type TokenInfo } from ".";
 import { IcTokenStandard, type IcTokenInfo } from "./ic";
 import { get_cached_data } from "~hooks/store";
 
 // IC
-import TOKEN_IC_ICP_SVG from "data-base64:~assets/svg/tokens/ic/ICP.min.svg";
+import TOKEN_IC_ICP_SVG from 'data-base64:~assets/svg/tokens/ic/ICP.min.svg';
 // CK
-import TOKEN_IC_CK_BTC_SVG from "data-base64:~assets/svg/tokens/ic/ck/ckBTC.min.svg";
-import TOKEN_IC_CK_ETH_SVG from "data-base64:~assets/svg/tokens/ic/ck/ckETH.min.svg";
-import TOKEN_IC_CK_USDT_SVG from "data-base64:~assets/svg/tokens/ic/ck/ckUSDT.min.svg";
-import TOKEN_IC_CK_USDC_SVG from "data-base64:~assets/svg/tokens/ic/ck/ckUSDC.min.svg";
+import TOKEN_IC_CK_BTC_SVG    from 'data-base64:~assets/svg/tokens/ic/ck/ckBTC.min.svg';
+import TOKEN_IC_CK_USDT_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckUSDT.min.svg';
+import TOKEN_IC_CK_USDC_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckUSDC.min.svg';
+import TOKEN_IC_CK_ETH_SVG    from 'data-base64:~assets/svg/tokens/ic/ck/ckETH.min.svg';
+import TOKEN_IC_CK_LINK_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckLINK.min.svg';
+import TOKEN_IC_CK_OCT_SVG    from 'data-base64:~assets/svg/tokens/ic/ck/ckOCT.min.svg';
+import TOKEN_IC_CK_PEPE_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckPEPE.min.svg';
+import TOKEN_IC_CK_EURC_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckEURC.min.svg';
+import TOKEN_IC_CK_SHIB_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckSHIB.min.svg';
+import TOKEN_IC_CK_XAUT_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckXAUT.min.svg';
+import TOKEN_IC_CK_WSTETH_SVG from 'data-base64:~assets/svg/tokens/ic/ck/ckWSTETH.min.svg';
+import TOKEN_IC_CK_UNI_SVG    from 'data-base64:~assets/svg/tokens/ic/ck/ckUNI.min.svg';
+import TOKEN_IC_CK_WBTC_SVG   from 'data-base64:~assets/svg/tokens/ic/ck/ckWBTC.min.svg';
 // SNS
 import TOKEN_IC_SNS_CHAT_PNG from "data-base64:~assets/svg/tokens/ic/sns/CHAT.png";
 import TOKEN_IC_SNS_ICS_PNG from "data-base64:~assets/svg/tokens/ic/sns/ICS.png";
-
 
 const PRESET_LOGO: Record<string, string> = {
     'ic#ryjl3-tyaaa-aaaaa-aaaba-cai': TOKEN_IC_ICP_SVG,
     // CK
     'ic#mxzaz-hqaaa-aaaar-qaada-cai': TOKEN_IC_CK_BTC_SVG,
-    'ic#ss2fx-dyaaa-aaaar-qacoq-cai': TOKEN_IC_CK_ETH_SVG,
     'ic#cngnf-vqaaa-aaaar-qag4q-cai': TOKEN_IC_CK_USDT_SVG,
     'ic#xevnm-gaaaa-aaaar-qafnq-cai': TOKEN_IC_CK_USDC_SVG,
+    'ic#ss2fx-dyaaa-aaaar-qacoq-cai': TOKEN_IC_CK_ETH_SVG,
+    'ic#g4tto-rqaaa-aaaar-qageq-cai': TOKEN_IC_CK_LINK_SVG,
+    'ic#ebo5g-cyaaa-aaaar-qagla-cai': TOKEN_IC_CK_OCT_SVG,
+    'ic#etik7-oiaaa-aaaar-qagia-cai': TOKEN_IC_CK_PEPE_SVG,
+    'ic#pe5t5-diaaa-aaaar-qahwa-cai': TOKEN_IC_CK_EURC_SVG,
+    'ic#fxffn-xiaaa-aaaar-qagoa-cai': TOKEN_IC_CK_SHIB_SVG,
+    'ic#nza5v-qaaaa-aaaar-qahzq-cai': TOKEN_IC_CK_XAUT_SVG,
+    'ic#j2tuh-yqaaa-aaaar-qahcq-cai': TOKEN_IC_CK_WSTETH_SVG,
+    'ic#ilzky-ayaaa-aaaar-qahha-cai': TOKEN_IC_CK_UNI_SVG,
+    'ic#bptq2-faaaa-aaaar-qagxq-cai': TOKEN_IC_CK_WBTC_SVG,
     // SNS
     'ic#2ouva-viaaa-aaaaq-aaamq-cai': TOKEN_IC_SNS_CHAT_PNG,
     'ic#ca6gz-lqaaa-aaaaq-aacwa-cai': TOKEN_IC_SNS_ICS_PNG,
@@ -32,7 +50,7 @@ export const get_token_logo_key = (token: {ic: {canister_id: string}}): string =
 }
 
 export const get_token_logo = async (info: CombinedTokenInfo): Promise<string | undefined> => {
-     const preset = match_combined_token_info(info, {
+    const preset = match_combined_token_info(info, {
         ic: (ic) => PRESET_LOGO[`ic#${ic.canister_id}`],
     });
     if (preset) return preset;
@@ -42,10 +60,19 @@ export const get_token_logo = async (info: CombinedTokenInfo): Promise<string | 
 
 const TOKEN_INFO_IC_ICP : IcTokenInfo = { canister_id: 'ryjl3-tyaaa-aaaaa-aaaba-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'Internet Computer', symbol: 'ICP', decimals:  8, fee: '10000' }; // fee 0.0001 ICP
 // CK
-const TOKEN_INFO_IC_CK_BTC : IcTokenInfo = { canister_id: 'mxzaz-hqaaa-aaaar-qaada-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckBTC',  symbol: 'ckBTC',  decimals:  8, fee:            '10' }; // fee 0.0000001 ckBTC
-const TOKEN_INFO_IC_CK_ETH : IcTokenInfo = { canister_id: 'ss2fx-dyaaa-aaaar-qacoq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckETH',  symbol: 'ckETH',  decimals: 18, fee: '2000000000000' }; // fee 0.000002 ckETH
-const TOKEN_INFO_IC_CK_USDT: IcTokenInfo = { canister_id: 'cngnf-vqaaa-aaaar-qag4q-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckUSDT', symbol: 'ckUSDT', decimals:  6, fee:         '10000' }; // fee 0.01 ckUSDT
-const TOKEN_INFO_IC_CK_USDC: IcTokenInfo = { canister_id: 'xevnm-gaaaa-aaaar-qafnq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckUSDC', symbol: 'ckUSDC', decimals:  6, fee:         '10000' }; // fee 0.01 ckUSDC
+const TOKEN_INFO_IC_CK_BTC    : IcTokenInfo = { canister_id: 'mxzaz-hqaaa-aaaar-qaada-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckBTC',    symbol: 'ckBTC',    decimals:  8, fee:                     '10' }; // fee 0.0000001 ckBTC
+const TOKEN_INFO_IC_CK_USDT   : IcTokenInfo = { canister_id: 'cngnf-vqaaa-aaaar-qag4q-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckUSDT',   symbol: 'ckUSDT',   decimals:  6, fee:                  '10000' }; // fee 0.01 ckUSDT
+const TOKEN_INFO_IC_CK_USDC   : IcTokenInfo = { canister_id: 'xevnm-gaaaa-aaaar-qafnq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckUSDC',   symbol: 'ckUSDC',   decimals:  6, fee:                  '10000' }; // fee 0.01 ckUSDC
+const TOKEN_INFO_IC_CK_ETH    : IcTokenInfo = { canister_id: 'ss2fx-dyaaa-aaaar-qacoq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckETH',    symbol: 'ckETH',    decimals: 18, fee:          '2000000000000' }; // fee 0.000002 ckETH
+const TOKEN_INFO_IC_CK_LINK   : IcTokenInfo = { canister_id: 'g4tto-rqaaa-aaaar-qageq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckLINK',   symbol: 'ckLINK',   decimals: 18, fee:        '100000000000000' }; // fee 0.0001 ckLINK
+const TOKEN_INFO_IC_CK_OCT    : IcTokenInfo = { canister_id: 'ebo5g-cyaaa-aaaar-qagla-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckOCT',    symbol: 'ckOCT',    decimals: 18, fee:      '34000000000000000' }; // fee 0.034 ckOCT
+const TOKEN_INFO_IC_CK_PEPE   : IcTokenInfo = { canister_id: 'etik7-oiaaa-aaaar-qagia-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckPEPE',   symbol: 'ckPEPE',   decimals: 18, fee: '1000000000000000000000' }; // fee 1000 ckPEPE
+const TOKEN_INFO_IC_CK_EURC   : IcTokenInfo = { canister_id: 'pe5t5-diaaa-aaaar-qahwa-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckEURC',   symbol: 'ckEURC',   decimals:  6, fee:                  '10000' }; // fee 0.01 ckEURC
+const TOKEN_INFO_IC_CK_SHIB   : IcTokenInfo = { canister_id: 'fxffn-xiaaa-aaaar-qagoa-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckSHIB',   symbol: 'ckSHIB',   decimals: 18, fee:  '100000000000000000000' }; // fee 100 ckSHIB
+const TOKEN_INFO_IC_CK_XAUT   : IcTokenInfo = { canister_id: 'nza5v-qaaaa-aaaar-qahzq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckXAUT',   symbol: 'ckXAUT',   decimals:  6, fee:                      '1' }; // fee 0.000001 ckXAUT
+const TOKEN_INFO_IC_CK_WSTETH : IcTokenInfo = { canister_id: 'j2tuh-yqaaa-aaaar-qahcq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckWSTETH', symbol: 'ckWSTETH', decimals: 18, fee:          '1000000000000' }; // fee 0.000001 ckWSTETH
+const TOKEN_INFO_IC_CK_UNI    : IcTokenInfo = { canister_id: 'ilzky-ayaaa-aaaar-qahha-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckUNI',    symbol: 'ckUNI',    decimals: 18, fee:       '1000000000000000' }; // fee 0.001 ckUNI
+const TOKEN_INFO_IC_CK_WBTC   : IcTokenInfo = { canister_id: 'bptq2-faaaa-aaaar-qagxq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ckWBTC',   symbol: 'ckWBTC',   decimals:  8, fee:                     '10' }; // fee 0.0000001 ckWBTC
 // SNS
 const TOKEN_INFO_IC_SNS_CHAT: IcTokenInfo = { canister_id: '2ouva-viaaa-aaaaq-aaamq-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'CHAT',          symbol: 'CHAT', decimals: 8, fee:  '100000' }; // fee 0.001 CHAT
 const TOKEN_INFO_IC_SNS_ICS : IcTokenInfo = { canister_id: 'ca6gz-lqaaa-aaaaq-aacwa-cai', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: 'ICPSwap Token', symbol:  'ICS', decimals: 8, fee: '1000000' }; // fee 0.01 ICS
@@ -63,10 +90,19 @@ export const DEFAULT_TOKEN_INFO: TokenInfo[] = [
 export const PRESET_ALL_TOKEN_INFO: TokenInfo[] = [
     { info: { ic: TOKEN_INFO_IC_ICP     }, tags: [TokenTag.ChainIc] },
     // CK
-    { info: { ic: TOKEN_INFO_IC_CK_BTC  }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
-    { info: { ic: TOKEN_INFO_IC_CK_ETH  }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
-    { info: { ic: TOKEN_INFO_IC_CK_USDT }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
-    { info: { ic: TOKEN_INFO_IC_CK_USDC }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_BTC    }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_USDT   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_USDC   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_ETH    }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_LINK   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_OCT    }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_PEPE   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_EURC   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_SHIB   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_XAUT   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_WSTETH }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_UNI    }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
+    { info: { ic: TOKEN_INFO_IC_CK_WBTC   }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },
     // SNS
     { info: { ic: TOKEN_INFO_IC_SNS_CHAT }, tags: [TokenTag.ChainIc, TokenTag.ChainIcSns] },
     { info: { ic: TOKEN_INFO_IC_SNS_ICS  }, tags: [TokenTag.ChainIc, TokenTag.ChainIcSns] },
---
+++ test/update-ck.ts
@@ -0,0 +1,106 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import BigNumber from 'bignumber.js';
+
+type TokenItem = {
+    ledger_canister_id: string;
+    token_type: string; // chain_key
+    icrc1_metadata: {
+        icrc1_fee: string;
+        icrc1_name: string;
+        icrc1_logo: string;
+        icrc1_symbol: string;
+        icrc1_decimals: string;
+    };
+};
+
+const main = async () => {
+    const response = await fetch(
+        'https://icrc-api.internetcomputer.org/api/v2/ledgers?limit=100&token_types=chain_key&include_total_supply_7d=true&network=mainnet&sort_by=-total_transactions_count_over_past_7d',
+    );
+    const json = await response.json();
+    const data = json.data as TokenItem[];
+
+    // save images
+    const items: {
+        token: TokenItem;
+        ext: string;
+        import_logo_name: string;
+        token_info_name: string;
+    }[] = [];
+    for (const d of data) {
+        const logo = d.icrc1_metadata.icrc1_logo;
+        let ext = (() => {
+            if (logo.startsWith('data:image/svg+xml')) return 'svg';
+            throw new Error(`unknown logo extension: ${logo}`);
+        })();
+        const name = `${d.icrc1_metadata.icrc1_symbol}.${ext}`;
+        const binary = Buffer.from(logo.replace(/^data:image\/(png|jpeg|jpg|svg\+xml);base64,/, ''), 'base64');
+        fs.writeFileSync(path.join(__dirname, '../src/assets/svg/tokens/ic/ck', name), binary);
+        const n = d.icrc1_metadata.icrc1_symbol.replace('ck', '').toUpperCase();
+        items.push({
+            token: d,
+            ext,
+            import_logo_name: `TOKEN_IC_CK_${n}_${ext.toUpperCase()}`,
+            token_info_name: `TOKEN_INFO_IC_CK_${n}`,
+        });
+    }
+
+    // print import
+    const max_logo_length = Math.max(...items.map((item) => item.token_info_name.length));
+    console.log(`============== import ==============`);
+    console.log(`// CK`);
+    for (const item of items) {
+        let spaces = '';
+        for (let i = 0; i < max_logo_length - item.import_logo_name.length - 1; i++) spaces += ' ';
+        console.log(
+            `import ${item.import_logo_name}${spaces} from 'data-base64:~assets/svg/tokens/ic/ck/${item.token.icrc1_metadata.icrc1_symbol}.${item.ext === 'svg' ? 'min.svg' : 'png'}';`,
+        );
+    }
+
+    // print preset
+    console.log(``);
+    console.log(`============== preset logo ==============`);
+    console.log(`    // CK`);
+    for (const item of items) {
+        console.log(`    'ic#${item.token.ledger_canister_id}': ${item.import_logo_name},`);
+    }
+
+    // print token info
+    const max_token_info_length = Math.max(...items.map((item) => item.token_info_name.length));
+    const max_decimals_length = Math.max(...items.map((item) => item.token.icrc1_metadata.icrc1_decimals.length));
+    const max_fee_length = Math.max(...items.map((item) => item.token.icrc1_metadata.icrc1_fee.length));
+    console.log(``);
+    console.log(`============== token info ==============`);
+    console.log(`// CK`);
+    for (const item of items) {
+        let token_info_spaces = '';
+        for (let i = 0; i < max_token_info_length - item.token_info_name.length; i++) token_info_spaces += ' ';
+        let decimals_spaces = '';
+        for (let i = 0; i < max_decimals_length - item.token.icrc1_metadata.icrc1_decimals.length; i++)
+            decimals_spaces += ' ';
+        let fee_spaces = '';
+        for (let i = 0; i < max_fee_length - item.token.icrc1_metadata.icrc1_fee.length; i++) fee_spaces += ' ';
+        console.log(
+            `const ${item.token_info_name}${token_info_spaces} : IcTokenInfo = { canister_id: '${item.token.ledger_canister_id}', standards: [IcTokenStandard.ICRC1, IcTokenStandard.ICRC2], name: '${item.token.icrc1_metadata.icrc1_symbol}',${token_info_spaces} symbol: '${item.token.icrc1_metadata.icrc1_symbol}',${token_info_spaces} decimals: ${decimals_spaces}${item.token.icrc1_metadata.icrc1_decimals}, fee: ${fee_spaces}'${item.token.icrc1_metadata.icrc1_fee}' }; // fee ${BigNumber(
+                item.token.icrc1_metadata.icrc1_fee,
+            )
+                .div(BigNumber(10).pow(BigNumber(item.token.icrc1_metadata.icrc1_decimals)))
+                .toFixed()} ${item.token.icrc1_metadata.icrc1_symbol}`,
+        );
+    }
+
+    // print preset all token info
+    console.log(``);
+    console.log(`============== preset all token info ==============`);
+    console.log(`    // CK`);
+    for (const item of items) {
+        let token_info_spaces = '';
+        for (let i = 0; i < max_token_info_length - item.token_info_name.length; i++) token_info_spaces += ' ';
+        console.log(
+            `    { info: { ic: ${item.token_info_name}${token_info_spaces} }, tags: [TokenTag.ChainIc, TokenTag.ChainIcCk] },`,
+        );
+    }
+};
+
+main();
