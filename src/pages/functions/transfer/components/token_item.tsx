1faace3d972c9c9204df7261aacf21a209375a8e
torin918 <torin_0101@163.com>
2025-03-10 14:05:32 +0800

    update: home switch wallet and settings address manage

--- src/pages/functions/transfer/components/token_item.tsx
+++ src/pages/functions/transfer/components/token_item.tsx
@@ -1,21 +1,70 @@
-import { useEffect, useState } from 'react';
+import BigNumber from 'bignumber.js';
+import { useEffect, useMemo, useState } from 'react';
 
 import type { GotoFunction } from '~hooks/memo/goto';
 import { match_combined_token_info, type TokenInfo } from '~types/tokens';
 import type { IcTokenInfo } from '~types/tokens/ic';
 import { get_token_logo } from '~types/tokens/preset';
 
-export const TransferShowToken = ({ goto, token }: { goto: GotoFunction; token: TokenInfo }) => {
+export const TransferShowToken = ({
+    goto,
+    token,
+    canisters,
+    ic_balances,
+    ic_prices,
+}: {
+    goto: GotoFunction;
+    token: TokenInfo;
+    canisters: string[];
+    ic_balances: (string | undefined)[];
+    ic_prices: [string | undefined, string | undefined][];
+}) => {
     return match_combined_token_info(token.info, {
-        ic: (ic) => <TransferShowTokenIc goto={goto} token={token} ic={ic} />,
+        ic: (ic) => (
+            <TransferShowTokenIc
+                goto={goto}
+                token={token}
+                ic={ic}
+                canisters={canisters}
+                balances={ic_balances}
+                prices={ic_prices}
+            />
+        ),
     });
 };
 
-const TransferShowTokenIc = ({ goto, token, ic }: { goto: GotoFunction; token: TokenInfo; ic: IcTokenInfo }) => {
+const TransferShowTokenIc = ({
+    goto,
+    token,
+    ic,
+    canisters,
+    balances,
+    prices,
+}: {
+    goto: GotoFunction;
+    token: TokenInfo;
+    ic: IcTokenInfo;
+    canisters: string[];
+    balances: (string | undefined)[];
+    prices: [string | undefined, string | undefined][];
+}) => {
     const [logo, setLogo] = useState<string>();
     useEffect(() => {
         get_token_logo(token.info).then(setLogo);
     }, [token]);
+
+    const [balance, price, price_changed_24h] = useMemo(() => {
+        const index = canisters.findIndex((c) => c == ic.canister_id);
+        if (index < 0) return [undefined, undefined, undefined];
+        return [balances[index], prices[index][0], prices[index][1]];
+    }, [ic, canisters, balances, prices]);
+
+    const usd = useMemo(() => {
+        if (balance === undefined) return undefined;
+        if (price === undefined) return undefined;
+        return BigNumber(balance).times(BigNumber(price)).div(BigNumber(10).pow(ic.decimals)).toFormat(2);
+    }, [balance, price, ic]);
+
     return (
         <div
             className="flex w-full cursor-pointer items-center justify-between rounded-xl bg-[#181818] p-[10px] transition duration-300 hover:bg-[#2B2B2B]"
@@ -25,14 +74,36 @@ const TransferShowTokenIc = ({ goto, token, ic }: { goto: GotoFunction; token: T
                 <img src={logo} className="h-10 w-10 rounded-full" />
                 <div className="ml-[10px]">
                     <strong className="block text-base text-[#EEEEEE]">{ic.symbol}</strong>
-                    <span className="text-xs text-[#999999]">$10.97</span>
-                    <span className="pl-2 text-xs text-[#00C431]">+2.8%</span>
-                    <span className="pl-2 text-xs text-[#FF2C40]">-2.87%</span>
+                    {price === undefined && (
+                        <span className="text-xs text-[#999999]">
+                            <span className="opacity-0">--</span>
+                        </span>
+                    )}
+                    {price !== undefined && (
+                        <>
+                            <span className="text-xs text-[#999999]">${BigNumber(price).toFormat(2)}</span>
+                            {price_changed_24h !== undefined && price_changed_24h.startsWith('-') && (
+                                <span className="pl-2 text-xs text-[#FF2C40]">
+                                    {BigNumber(price_changed_24h).toFormat(2)}%
+                                </span>
+                            )}
+                            {price_changed_24h !== undefined && !price_changed_24h.startsWith('-') && (
+                                <span className="pl-2 text-xs text-[#00C431]">
+                                    +{BigNumber(price_changed_24h).toFormat(2)}%
+                                </span>
+                            )}
+                        </>
+                    )}
                 </div>
             </div>
             <div className="flex-end flex shrink-0 flex-col">
-                <strong className="block text-right text-base text-[#EEEEEE]">800.12</strong>
-                <span className="text-right text-xs text-[#999999]">$8,160.91</span>
+                <strong className="block text-right text-base text-[#EEEEEE]">
+                    {balance === undefined && <span className="opacity-0">--</span>}
+                    {balance !== undefined && <>{BigNumber(balance).div(BigNumber(10).pow(ic.decimals)).toFormat(2)}</>}
+                </strong>
+                <span className="text-right text-xs text-[#999999]">
+                    {usd === undefined ? <span className="opacity-0">--</span> : <>${usd}</>}
+                </span>
             </div>
         </div>
     );
