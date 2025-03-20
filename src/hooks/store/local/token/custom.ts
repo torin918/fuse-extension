d573c2901564087badff5c6f835c6d0e9b2bf554
torin918 <torin_0101@163.com>
2025-03-20 12:28:54 +0800

    update: search token and custom token page

--- src/hooks/store/local/token/custom.ts
+++ src/hooks/store/local/token/custom.ts
@@ -4,7 +4,16 @@ import type { Storage } from '@plasmohq/storage';
 
 import { useCachedStoreData0, type DataMetadata0 } from '~hooks/meta/metadata-0';
 import { resort_list, type ResortFunction } from '~lib/utils/sort';
-import { get_token_symbol, is_same_token_info, TokenTag, type CustomTokens, type TokenInfo } from '~types/tokens';
+import { match_chain, type Chain } from '~types/chain';
+import {
+    get_token_symbol,
+    is_same_token_info,
+    TokenTag,
+    type AnyTokenInfo,
+    type CombinedTokenInfo,
+    type CustomTokens,
+    type TokenInfo,
+} from '~types/tokens';
 import type { IcTokenInfo } from '~types/tokens/chain/ic';
 import { is_known_token } from '~types/tokens/preset';
 
@@ -34,18 +43,58 @@ export const useTokenInfoCustomInner2 = (
     DataType,
     {
         pushCustomIcToken: (token: IcTokenInfo) => Promise<TokenInfo | undefined>;
+        pushCustomToken: (token: AnyTokenInfo, network: Chain) => Promise<TokenInfo | undefined>;
         removeCustomToken: (token: TokenInfo) => Promise<void>;
         resortCustomToken: ResortFunction;
     },
 ] => {
     const [custom, setCustom] = useTokenInfoCustomInner(storage);
 
-    // push
+    // push ic
     const pushCustomIcToken = useCallback(
         async (token: IcTokenInfo): Promise<TokenInfo | undefined> => {
             if (!storage) return undefined;
 
             const combined: TokenInfo = { info: { ic: token }, tags: [TokenTag.ChainIcCustom] };
+            if (is_known_token(combined) || !!custom.find((c) => is_same_token_info(c.token, combined)))
+                throw new Error(`Token ${get_token_symbol(combined)} is exist`);
+
+            const now = Date.now();
+            const new_custom: CustomTokens = [...custom, { created: now, updated: now, token: combined }];
+
+            await setCustom(new_custom);
+
+            return combined;
+        },
+        [storage, custom, setCustom],
+    );
+
+    const getChainKeyAndTag = (chain: Chain): { key: string; tag: TokenTag } => {
+        return match_chain<{ key: string; tag: TokenTag }>(chain, {
+            ic: () => ({ key: 'ic', tag: TokenTag.ChainIcCustom }),
+            ethereum: () => ({ key: 'ethereum', tag: TokenTag.ChainEthereumCustom }),
+            ethereum_test_sepolia: () => ({
+                key: 'ethereum_test_sepolia',
+                tag: TokenTag.ChainEthereumTestSepoliaCustom,
+            }),
+            polygon: () => ({ key: 'polygon', tag: TokenTag.ChainPolygonCustom }),
+            polygon_test_amoy: () => ({ key: 'polygon_test_amoy', tag: TokenTag.ChainPolygonTestAmoyCustom }),
+            bsc: () => ({ key: 'bsc', tag: TokenTag.ChainBscCustom }),
+            bsc_test: () => ({ key: 'bsc_test', tag: TokenTag.ChainBscTestCustom }),
+        });
+    };
+
+    // push token
+    const pushCustomToken = useCallback(
+        async (token: AnyTokenInfo, network: Chain): Promise<TokenInfo | undefined> => {
+            if (!storage) return undefined;
+
+            const nameTag = getChainKeyAndTag(network);
+            if (!nameTag) throw new Error(`Unknown chain`);
+
+            const tokenInfo = { [nameTag.key]: token } as CombinedTokenInfo;
+
+            const combined: TokenInfo = { info: tokenInfo, tags: [nameTag.tag] };
             if (is_known_token(combined) || !!custom.find((c) => is_same_token_info(c.token, combined)))
                 throw new Error(`Token ${get_token_symbol(combined)} is exist`);
 
@@ -90,5 +139,5 @@ export const useTokenInfoCustomInner2 = (
         [storage, custom, setCustom],
     );
 
-    return [custom, { pushCustomIcToken, removeCustomToken, resortCustomToken }];
+    return [custom, { pushCustomIcToken, pushCustomToken, removeCustomToken, resortCustomToken }];
 };
